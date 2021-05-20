4c04d0e80d8098c4b68e6250be796efb230c4ea7
LuisK <luishsk93@gmail.com>
2021-05-20 13:35:37 -0300

    Exercicio 9 - Imprimir cada elemento de um array dividido por 2

---
+++ exercises/bloco_4/dia_2/exercicios_aprofundamento/exercicio_9.js
@@ -0,0 +1,11 @@
+// Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 
+
+let numbers = [];
+
+for (i = 1; i <= 25; i += 1){
+    numbers.push(i);
+}
+
+for (number of numbers){
+    console.log(number/2);
+}
