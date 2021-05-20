e03886658f3bc0d977f86115edd78bc54865c6ff
LuisK <luishsk93@gmail.com>
2021-05-20 13:21:39 -0300

    Exercicio 5 -Utilizar for para descubrir o maior valor contido no array

---
+++ exercises/bloco_4/dia_2/exercicios_aprofundamento/exercicio_5.js
@@ -0,0 +1,13 @@
+// Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+maior = -Infinity;
+
+for (number of numbers){
+    if (number > maior){
+        maior = number
+    }
+}
+
+console.log(maior);
