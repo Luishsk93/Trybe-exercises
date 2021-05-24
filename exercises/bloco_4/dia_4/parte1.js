648f5d230011ecc4dd56900942a9aecefe717110
Luishsk93 <hyungsunkangkr@gmail.com>
2021-05-24 18:04:24 -0300

    Define um segundo objeto com a mesma estrutua e imprime os valores de cada objeto de acordo com cad a uma das chaves

--- exercises/bloco_4/dia_4/parte1.js
+++ exercises/bloco_4/dia_4/parte1.js
@@ -11,6 +11,7 @@ let info = {
 
 console.log("Bem-vinda, " + info.personagem);
 console.log('\n');
+
 // Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
 
 info.recorrente = 'Sim';
@@ -24,6 +25,7 @@ for (key in info){
 }
 
 console.log('\n');
+
 // Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
 
 for (key in info){
@@ -31,5 +33,29 @@ for (key in info){
 }
 
 console.log('\n');
+
 // Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
 
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (key in info){
+  if (key == 'recorrente'){
+    if (info[key] == 'Sim' && info2[key] == 'Sim'){
+      console.log('Ambos recorrentes');
+    }
+    else if (info[key] == 'Nao' && info2[key] == 'Nao'){
+      console.log('Ambos nao recorrentes');
+    }
+    else{
+      console.log(info[key] + " e " + info2[key]);
+    }
+  }
+  else{
+    console.log(info[key] + " e " + info2[key]);
+  }
+}
