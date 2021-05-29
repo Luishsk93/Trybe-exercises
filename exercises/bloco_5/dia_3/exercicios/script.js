917c54dcfdd7bc57e18e86654190a05ceee4c41b
Luishsk93 <hyungsunkangkr@gmail.com>
2021-05-29 14:08:04 -0300

    adiciona os dias e as suas respectivas classes

--- exercises/bloco_5/dia_3/exercicios/script.js
+++ exercises/bloco_5/dia_3/exercicios/script.js
@@ -14,3 +14,27 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  function createDaysOfTheMonth() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const DaysList = document.querySelector('#days');
+  
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+      const days = dezDaysList[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+      if (days == '24' || days == '31') {
+        dayListItem.className = 'day holiday';
+      } else if (days == '4' || days == '11' || days == '18') {
+        dayListItem.className = 'day friday';
+      } else if (days == '25') {
+        dayListItem.className = 'day holiday friday';  
+      } else {
+        dayListItem.className = 'day';
+      }
+      DaysList.appendChild(dayListItem);
+    }
+  };
+    
+  
+  createDaysOfTheMonth();
