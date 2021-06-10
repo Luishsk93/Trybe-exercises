1c81b4ec378e7917f0f65c6d70538303f9b608c2
Luishsk93 <hyungsunkangkr@gmail.com>
2021-06-10 00:45:36 -0300

    Adiciona um botao que valida todas as informacoes pelo Java, e cria uma div com os inputs e uma div com mensagem de erro

--- exercises/bloco_6/dia_1/exercicios/script.js
+++ exercises/bloco_6/dia_1/exercicios/script.js
@@ -1,5 +1,20 @@
 const listaEstados = ["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"];
 const estados = document.getElementById('estado');
+const data = document.getElementById('dataInicio');
+const botaoSubmeter = document.getElementById('botaoSubmeter');
+const casa = document.getElementById('tipo-moradia-casa');
+const ap = document.getElementById('tipo-moradia-ap');
+const listaIds = ['full-name', 'email', 'cpf', 'endereco', 'cidade', 'Cargo', 'DescricaoCargo', 'curriculo', 'dataInicio'];
+let validado = false;
+let validadoData = false;
+
+botaoSubmeter.addEventListener('click', validaData);
+botaoSubmeter.addEventListener('click',paraAcaoPadrao);
+botaoSubmeter.addEventListener('click', validarTextos);
+botaoSubmeter.addEventListener('click', validarRadio);
+botaoSubmeter.addEventListener('click', dadosConsolidados)
+botaoSubmeter.addEventListener('click', erro)
+
 for (let index = 0; index < listaEstados.length; index += 1) {
   const opcaoEstado = document.createElement('option');
   opcaoEstado.value = listaEstados[index];
@@ -7,19 +22,77 @@ for (let index = 0; index < listaEstados.length; index += 1) {
   estados.appendChild(opcaoEstado);
 }
 
-const data = document.getElementById('dataInicio')
 function validaData(event) {
-  let x = event.target
-  if (x.value !== "") {
-    if (parseInt(x.value[0] + x.value[1]) < 0 || parseInt(x.value[0] + x.value[1]) > 31 || parseInt(x.value[3] + x.value[4]) < 0 || parseInt(x.value[3] + x.value[4]) > 12 || x.value[6] === "-") {
+  let x = data.value;
+  if (data.value !== "") {
+    if (parseInt(x[0] + x[1]) < 0 || parseInt(x[0] + x[1]) > 31 || parseInt(x[3] + x[4]) < 0 || parseInt(x[3] + x[4]) > 12 || x[6] === "-") {
     alert('Data inconsistente');
+    validadoData = false;
+    }
+    else {
+      validadoData = true;
     }
   }
 }
-data.addEventListener('input', validaData);
 
-const botaoSubmeter = document.getElementById('botaoSubmeter');
-
-function previnir(event) {
-  
+// referencia https://developer.mozilla.org/pt-BR/docs/Web/API/Event/preventDefault
+function paraAcaoPadrao(event) {
+    event.preventDefault();
 }
+
+function validarTextos() {
+  for (let i = 0; i < listaIds.length; i += 1) {
+    let x = document.getElementById(listaIds[i]);
+    if (x.length > x.maxLength || x.value === "") {
+      validado = false;
+    }
+    else {
+      validado = true;
+    }
+  }
+}
+
+function validarRadio() {
+  if (casa.checked === false && ap.checked === false) {
+    validado = false;
+  }
+  else {
+    validado = true;
+  }
+}
+
+function dadosConsolidados() {
+  if (validado === true && validadoData === true) {
+    const divConsolidado = document.createElement('div');
+    document.body.appendChild(divConsolidado);
+    for (let i = 0; i < listaIds.length; i += 1) {
+      let y = document.getElementById(listaIds[i]);
+      var texto = texto + " " + y.value;
+    }
+    if (casa.checked === true) {
+      texto = texto + " " + 'casa';
+    } else if (ap.checked === true) {
+      texto = texto + " " + 'apartamento';
+    }
+    divConsolidado.innerText = texto;
+  }
+}
+
+function erro() {
+  if (validado === false && validadoData === true) {
+    let texto = "Dados nao validados";
+    const divErro = document.createElement('div');
+    document.body.appendChild(divErro);
+    divErro.innerText = texto;
+  } else if (validado === true && validadoData === false) {
+    const divErro = document.createElement('div');
+    document.body.appendChild(divErro);
+    let texto = "Data invalida";
+    divErro.innerText = texto;
+  } else if (validado === false && validadoData === false) {
+    const divErro = document.createElement('div');
+    document.body.appendChild(divErro);
+    let texto = "Dados nao validados e Data invalida";
+    divErro.innerText = texto;
+  }
+} 
