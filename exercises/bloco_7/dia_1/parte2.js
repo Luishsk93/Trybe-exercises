2b9f106b52fc667b53a08aa24c7f077b72d69c81
Luishsk93 <hyungsunkangkr@gmail.com>
2021-06-17 21:57:23 -0300

    Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string

--- exercises/bloco_7/dia_1/parte2.js
+++ exercises/bloco_7/dia_1/parte2.js
@@ -30,16 +30,34 @@ const retornarMaiorPalavra = (frase) => {
 
 console.log(retornarMaiorPalavra('Antônio foi no banheiro e não sabemos o que aconteceu'));
 
-// Crie uma página que contenha:
-// Um botão ao qual será associado um event listener ;
-// Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
-// Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
 
-const botao = document.getElementById('botao');
-let clickCount = 0;
-const p = document.getElementsByTagName('p')[0];
-botao.addEventListener('click', (event) => {
-  clickCount += 1;
-  p.innerText = clickCount;
-})
+
+// Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
+// Exemplo:
+// String determinada: "Tryber x aqui!"
+// Parâmetro: "Bebeto"
+// Retorno: "Tryber Bebeto aqui!"
+
+const substituirPalavra = (palavra) => {
+  let frase = "Tryber x aqui!";
+  let fraseNova = '';
+  for (let i = 0; i < frase.length; i += 1) {
+    if (frase[i] === 'x') {
+      fraseNova = fraseNova + palavra;
+    } else {
+      fraseNova = fraseNova + frase[i];
+    }
+  }
+  return fraseNova;
+}
+console.log(substituirPalavra('hyung'))
+
+// Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
+
+
+
+// Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
+// Exemplo: "Tryber x aqui! Minhas cinco principais habilidades são:
+// JavaScript;
+// HTML; ... #goTrybe".
 
