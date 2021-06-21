fc05f9c373916d0b9d9c90f8eb1e1e07d8c48d5b
Luishsk93 <hyungsunkangkr@gmail.com>
2021-06-21 18:40:29 -0300

    testar funcao myRemove, que retorna lista

--- exercises/bloco_7/dia_3/parte1.js
+++ exercises/bloco_7/dia_3/parte1.js
@@ -1,4 +1,4 @@
-// A função sum(a, b) retorna a soma do parâmetro a com o b
+// 1. A função sum(a, b) retorna a soma do parâmetro a com o b
 // Teste se o retorno de sum(4, 5) é 9
 // Teste se o retorno de sum(0, 0) é 0
 // Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
@@ -15,6 +15,28 @@ function sum(a, b) {
 }
 
 // implemente seus testes aqui
-assert.strictEqual(sum(4, 5), 9);
-assert.strictEqual(sum(0, 0), 0);
-assert.strictEqual(sum(4, "5"), 9);
+// assert.strictEqual(sum(4, 5), 9);
+// assert.strictEqual(sum(0, 0), 0);
+// assert.strictEqual(sum(4, "5"), 9);
+
+// 2. A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// Verifique se o array passado por parâmetro não sofreu alterações
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemove([1, 2, 3, 4]), [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
