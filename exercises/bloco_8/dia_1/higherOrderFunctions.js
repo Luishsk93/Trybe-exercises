c071459ac1c9184620ac121cce8704bd0885adb3
Luishsk93 <hyungsunkangkr@gmail.com>
2021-06-23 18:53:57 -0300

    Exercicio 1

---
+++ exercises/bloco_8/dia_1/higherOrderFunctions.js
@@ -0,0 +1,21 @@
+// 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+
+const newEmployees = (hiredPerson) => {
+    const employees = {
+      id1: hiredPerson('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: hiredPerson('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: hiredPerson('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    console.log(employees);
+  };
+
+const hiredPerson = (name) => {
+// Referencia https://stackoverflow.com/questions/5963182/how-to-remove-spaces-from-a-string-using-javascript. \s = space, g= global (match all)
+  personalInfo = {
+    nomeCompleto: name,
+    email: `${name.replace(/\s/g, '')}@trybe.com`,
+  };
+  return personalInfo;
+}
+
+newEmployees(hiredPerson)
