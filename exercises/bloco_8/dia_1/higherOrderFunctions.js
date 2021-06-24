4fbd00d771fb64937e18022a8276d466e3a13760
Luishsk93 <hyungsunkangkr@gmail.com>
2021-06-23 22:03:13 -0300

    Exercicio 3

--- exercises/bloco_8/dia_1/higherOrderFunctions.js
+++ exercises/bloco_8/dia_1/higherOrderFunctions.js
@@ -35,3 +35,29 @@ const checkResult = (randomNumber,luckNumber) => {
     return 'Tente novamente';
   }
 }
+
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const countPoints = (rightAnswers, studentAnswers, checkAnswers) => checkAnswers(rightAnswers, studentAnswers);
+
+const checkAnswers = (rightAnswers, studentAnswers) => {
+  let count = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    if (studentAnswers[index] === 'N.A') {
+      count = count;
+    } else {
+      if (studentAnswers[index] === rightAnswers[index]) {
+        count += 1;
+      } else {
+        count -= 0.5;
+      }
+    }
+  }
+  console.log(count);
+}
+
+countPoints(rightAnswers, studentAnswers, checkAnswers)
