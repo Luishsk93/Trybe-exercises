0094cb86fec0fde4ceedbb748feb309ade995b02
Luishsk93 <hyungsunkangkr@gmail.com>
2021-07-06 23:32:46 -0300

    Exercicio 1

--- exercises/bloco_9/dia_1/exercise1.js
+++ exercises/bloco_9/dia_1/exercise1.js
@@ -1,4 +1,5 @@
 // Dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+// A, B, C
 
 const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
 `${name} is ${value} ${measurementUnit} apart from the Sun`;
