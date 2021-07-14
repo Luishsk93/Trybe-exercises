9dde50798ca646d18b9257785d4e31b7d89ec6b1
Luishsk93 <hyungsunkangkr@gmail.com>
2021-07-14 17:28:12 -0300

    Exercicio 2

--- exercises/bloco_10/dia_2/testesAssincronos.test.js
+++ exercises/bloco_10/dia_2/testesAssincronos.test.js
@@ -1,7 +1,18 @@
-const { uppercase } = require('./testesAssincronos');
+const { uppercase, findUserById, getUserName } = require('./testesAssincronos');
 
 test('Ao chamar a função uppercase,', () => {
   uppercase('abacaxi', (result) => {
     expect(result).toBe('ABACAXI');
   })
+});
+
+describe('Ao chamar a funcao findUserById,', () => {
+  it('verifica se o usuario foi encontrado', () => {
+    findUserById(4).then( user => {
+      expect(user.name).toEqual('Mark');
+    });
+  });
+  it('verifica se o usuario nao foi encontrado', () => {
+    return expect(findUserById(6)).rejects.toEqual({ error: 'User with ' + 6 + ' not found.' })
+  });
 });
