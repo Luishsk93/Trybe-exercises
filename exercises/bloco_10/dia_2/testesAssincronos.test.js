95f02ddcdb97abc1b9175f248e50303ddeb91a4e
Luishsk93 <hyungsunkangkr@gmail.com>
2021-07-14 17:37:53 -0300

    Exercicio 3

--- exercises/bloco_10/dia_2/testesAssincronos.test.js
+++ exercises/bloco_10/dia_2/testesAssincronos.test.js
@@ -16,3 +16,19 @@ describe('Ao chamar a funcao findUserById,', () => {
     return expect(findUserById(6)).rejects.toEqual({ error: 'User with ' + 6 + ' not found.' })
   });
 });
+
+describe('Testando com async/await', () => {
+  describe('Ao chamar a funcao findUserById,', () => {
+    it('verifica se o usuario foi encontrado', async () => {
+      const user = await findUserById(5);
+      expect(user.name).toEqual('Paul');
+    });
+    it('verifica se o usuario foi encontrado', async () => {
+      try {
+        await findUserById(8);
+      } catch (error) {
+        expect(error).toEqual({ error: 'User with ' + 8 + ' not found.' });
+      }
+    });
+  });
+});
