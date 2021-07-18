f4c210b122b68d36f07dd0f9ab41134c37c540d3
Luishsk93 <hyungsunkangkr@gmail.com>
2021-07-18 15:18:12 -0300

    Refatorando

--- front-end/bloco_11/dia_2/fixation-exercises-11-2/src/App.js
+++ front-end/bloco_11/dia_2/fixation-exercises-11-2/src/App.js
@@ -1,5 +1,7 @@
 // arquivo App.js, criado pelo create-react-app, modificado
 import React from 'react';
+import './App.css';
+import Image from './Image'
 import Order from './Order';
 
 class App extends React.Component {
@@ -28,8 +30,8 @@ class App extends React.Component {
       <div className="App">
         <h1> Orders recently created </h1>
          {/*  adicione os componentes aqui */}
-        <Order order={ headphone }/>
-        <Order order={ energyDrink }/>
+         <Order order={ headphone, energyDrink }/>
+         <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText ="Cute cat staring"></Image>
       </div>
     );
   }
